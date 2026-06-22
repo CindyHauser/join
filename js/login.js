ee58c182557ef1ab9fb01d48e31fd7560e6f1096
KevinEberheim <kevin.eberheim@gmail.com>
2026-06-22 16:26:27 +0200

    change login function to smaller funktions

--- js/login.js
+++ js/login.js
@@ -1,18 +1,14 @@
 const BASE_URL = "https://join3195-7c673-default-rtdb.europe-west1.firebasedatabase.app/"
 
 const loginForm = document.getElementById('loginForm');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const errorMessageElement = document.getElementById('loginError');
 
 loginForm.addEventListener('submit', async (event) => {
     event.preventDefault();
-
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
-    const errorMessageElement = document.getElementById('loginError');
     errorMessageElement.textContent = '';
-    
-    const users = await fetch(BASE_URL + "user.json");
-    const userData = await users.json();
-    const user = Object.values(userData).find(u => u.email === email.value && u.password === password.value);
+    const user = await checkUserExists();
 
     if (user) {
         email.value = '';
@@ -23,3 +19,10 @@ loginForm.addEventListener('submit', async (event) => {
         password.value = '';
     }
 });
+
+async function checkUserExists() {
+    const users = await fetch(BASE_URL + "user.json");
+    const userData = await users.json();
+    const user = Object.values(userData).find(u => u.email === email.value);
+    return user;
+}
