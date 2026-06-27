f0eb97c8fbd9ac46603d57e9a282e6768e1444cb
Arnesto <arnestoarnesto94@gmail.com>
2026-06-27 09:19:52 +0200

    board html visual drag and drop first update

--- js/bordeUserInterface.js
+++ js/bordeUserInterface.js
@@ -8,20 +8,15 @@ const cardDragged = (event) => {
         "cardState": element.id
     }
     event.dataTransfer.setData("application/json", JSON.stringify(cardData))
-    console.log(cardData);
-
 }
 
 const cardDropped = (event) => {
     event.preventDefault()
     const element = event.target.closest('.progress-tasks')
     const dropZone = element.querySelector('.drag-and-drop-tasks')
-
     const preludeStringifyData = event.dataTransfer.getData("application/json")
     const cardData = JSON.parse(preludeStringifyData)
     cardData.cardState = element.id
-    console.log(cardData);
-
     const draggedCard = document.getElementById(cardData.cardId)
     renderDragzoneVisual(dropZone, draggedCard)
 }
